b8e7555368e6b0c696bf828a5125d17ebd757529
riki datul <riki@gmail.com>
2023-05-14 00:54:50 +0700

    UPLOAD

--- lib/main.dart
+++ lib/main.dart
@@ -8,140 +8,184 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'UTS Mobile Programming',
+      title: 'nama lu pada',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Riki Datul Fajar '),
-        ),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
+          backgroundColor: Colors.white,
+          body: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 4.0,
+                  ),
+                  borderRadius: BorderRadius.zero),
+              child: Column(children: [
+                AppBar(
+                  title: Text("Shoes", style: TextStyle(color: Colors.black)),
+                  actions: [Image(image: NetworkImage("orang"))],
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Colors.redAccent,
                   child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 192, 209, 173),
-                    child: Text("one"),
+                    child: ListTile(
+                      title: Text('Nike SB Zoom Blazer Mid Premium'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 3.0),
+                          Text(
+                            '',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 3.0),
+                          Text(
+                            '\u00A3 8,795',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: Image(image: NetworkImage("sepatu")),
+                    ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Colors.lightBlue,
                   child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 170, 224, 177),
-                    child: Text("two"),
+                    child: ListTile(
+                      title: Text('Nike Air Zoom Pegasus'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 3.0),
+                          Text(
+                            'Mens road running shoes',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 3.0),
+                          Text(
+                            '\u00A3 9,995',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: Image(image: NetworkImage("sepatu")),
+                    ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Colors.lightGreen,
                   child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 144, 211, 144),
-                    child: Text("three"),
+                    child: ListTile(
+                      title: Text('Nike Zoom X Vapor Fly'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 3.0),
+                          Text(
+                            'Mens road racing shoes',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 3.0),
+                          Text(
+                            '\u00A3 19,695',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: Image(image: NetworkImage("sepatu")),
+                    ),
                   ),
                 ),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Colors.grey,
                   child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 119, 173, 128),
-                    child: Text("four"),
+                    child: ListTile(
+                      title: Text('Nike Air Force 1 S50'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 3.0),
+                          Text(
+                            'Older kids shoe',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 3.0),
+                          Text(
+                            '\u00A3 6,295',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: Image(image: NetworkImage("sepatu")),
+                    ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Colors.amber,
                   child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 113, 161, 112),
-                    child: Text("five"),
+                    child: ListTile(
+                      title: Text('Nike Waffle One'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 3.0),
+                          Text(
+                            'Mens Shoe',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 3.0),
+                          Text(
+                            '\u00A3 8,295',
+                            style: TextStyle(
+                              fontSize: 10.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      trailing: Image(image: NetworkImage("sepatu")),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 82, 151, 91),
-                    child: Text("six"),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 71, 128, 69),
-                    child: Text("seven"),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 60, 145, 71),
-                    child: Text("eight"),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 60, 126, 54),
-                    child: Text("nine"),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 202, 228, 199),
-                    child: Text("ten"),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 182, 238, 191),
-                    child: Text("eleven"),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 100,
-                    width: 100,
-                    color: Color.fromARGB(255, 175, 226, 145),
-                    child: Text("twelve"),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+                )
+              ]))),
     );
   }
 }
